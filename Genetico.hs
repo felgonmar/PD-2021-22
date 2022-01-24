efdd1f2f24e517b133618c0b695f94cd5da34c84
felgonmar <felixgm1997@gmail.com>
2022-01-24 22:19:24 +0100

    docu

---
+++ Genetico.hs
@@ -0,0 +1,42 @@
+import System.Random
+
+
+type SolucionReinas = (Int, Tablero)
+
+type Tablero = [[Int]]
+
+generaPoblacion :: Int -> IO [[Double]]
+generaPoblacion n = do
+    individuo <- generaIndividuo (n*n)
+    return (parteLista individuo n)
+
+generaIndividuo :: Int -> IO [Double]
+generaIndividuo n = do
+  gen <- newStdGen
+  let xs = randomRs (0,1) gen
+  return (transforma (take n xs))
+
+transforma :: [Double] -> [Double]
+transforma xs = [if (x > 0.9) then 1 else 0 | x <- xs]
+  
+parteLista :: [a] -> Int -> [[a]]
+parteLista [] _ = []
+parteLista xs n = (take n xs) : parteLista (drop n xs) n
+
+
+muta::[[Int]]->[[Int]]
+muta [] = []
+muta (x:xs) = (mutaAux x):(muta xs)
+
+mutaAux:: [Int] -> [Int]
+mutaAux xs 
+            | (sum xs) == 1 = []
+            | (sum xs) == 0 = []
+ 
+
+fitness:: [Int] -> Int
+fitness xs 
+    | r < 45 = r
+    | otherwise = 9283489
+    where r = sum xs
+
