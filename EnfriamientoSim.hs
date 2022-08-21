4dde6451e244a2979ba85ed0a89789344c67b076
AlvaroMaya <varo9747@gmail.com>
2022-08-22 01:39:13 +0200

    QUEDA TERMINAR ENFRIAMIENTO_SIMULADO PERO POR AHORA COMPILA

--- EnfriamientoSim.hs
+++ EnfriamientoSim.hs
@@ -20,8 +20,55 @@ import System.IO
 -- ===================================
 --      VARIABLES AUXILIARES
 -- ===================================
+--ESTAS VARIABLES DEBERAN SER LLAMADAS DESDE LA PARTE DE SOL AL COMIENZO DEL ALGORITMO
 pesoMochila::Int
 pesoMochila = 5
+--En cada iteracion se generara el vecino 
+nIteraciones:: Int
+nIteraciones = 10
+type Temperatura = Double
+tempInicial::Temperatura
+tempInicial=100
+tempFinal::Temperatura
+tempFinal=1
+coolingRate::Temperatura
+coolingRate=0.1
+valoracionInicial::Int
+valoracionInicial= -1
+
+
+-- ===================================
+--      FUNCIONES PRINCIPAL
+-- ===================================
+enfriamiento_simulado::Double->Double->Double --tInicial, tFinal, coolingRate
+                        ->Int --nIter
+                             ->Int --valoracionInicial 0
+                                  ->[Objeto]->[Objeto] --poblacion inicial/lista de la compra/solucion inicial
+                                            ->Int --Peso limite de la mochila
+                                                  ->IO() --nuestra solucion la guardamos en un fichero, por tanto return()
+enfriamiento_simulado tInicial tFinal cRate nIter val lista solInicial pesoMoch  = do
+  if (tInicial < tFinal)
+    then do
+      if (nIter /= 0)
+        then do
+          --la expresion <- me sirve para trabajar con datos no IO
+          let nIter=nIter - 1
+          vecino <- comenzar_elegidos pesoMoch lista [] 
+          let valorVecino = fromIntegral $ funcion_valor vecino
+          let delta = fromIntegral $ val - valorVecino 
+          random<-randomNumber
+          if (delta < 0 ||prob_aceptacion delta tempInicial random)
+            then do
+                --let val = valorVecino
+                --let solInicial = vecino
+                enfriamiento_simulado tInicial tFinal cRate nIter val lista solInicial pesoMochila
+
+            else enfriamiento_simulado tInicial tFinal cRate nIter val lista solInicial pesoMochila
+       --en el siguiente else tenemos que hacer tInicial=tInicial*(1-cRAte)       
+        else do
+          let tInicial'=tInicial*(1-cRate)
+          enfriamiento_simulado tInicial' tFinal cRate nIteraciones val lista solInicial pesoMochila
+    else return ()
 -- ===================================
 --      FUNCIONES AUXILIARES
 -- ===================================
@@ -61,5 +108,15 @@ individuo lista = do
 randomIndice :: [a] -> IO Int
 randomIndice [] = error "Lista vacia"
 randomIndice list = getStdRandom $ randomR (0, length list - 1)
+
+randomNumber :: IO Double
+randomNumber = getStdRandom $ randomR (0.001, 1)
+
+--La formula es exp^(-delta/temperatura)
+prob_aceptacion:: Double->Double->Double->Bool
+prob_aceptacion delta temp random =  (op > random)
+    where op = (exp alt) 
+          delta' = 0 - delta 
+          alt = ( delta' / temp) 
 
 
