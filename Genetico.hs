a59ce0795114ebba87e451d96beb5d4e8f3ae042
felgonmar <felixgm1997@gmail.com>
2022-09-11 17:56:14 +0200

    reinas done

--- Genetico.hs
+++ Genetico.hs
@@ -1,9 +1,12 @@
 import System.Random
 
+-- tipo solucionesReinas será lo que valga la función fitness + el tablero 
+type SolucionReinas = (Double, Tablero)
 
-type SolucionReinas = (Int, Tablero)
+probabilidad = 0.8
+-- el tablero constiturá de un array de arrays de double (porque lo necesitamos para random) donde si es 1, es la posición de la reina
+type Tablero = [[Double]]
 
-type Tablero = [[Int]]
 
 generaPoblacion :: Int -> IO [[Double]]
 generaPoblacion n = do
@@ -14,29 +17,36 @@ generaIndividuo :: Int -> IO [Double]
 generaIndividuo n = do
   gen <- newStdGen
   let xs = randomRs (0,1) gen
-  return (transforma (take n xs))
+  return (transforma (take n xs) probabilidad)
 
-transforma :: [Double] -> [Double]
-transforma xs = [if (x > 0.9) then 1 else 0 | x <- xs]
+transforma :: [Double] ->Double -> [Double]
+transforma xs n = [if (x > n) then 1 else 0 | x <- xs] 
   
 parteLista :: [a] -> Int -> [[a]]
 parteLista [] _ = []
 parteLista xs n = (take n xs) : parteLista (drop n xs) n
 
 
-muta::[[Int]]->[[Int]]
+muta::[[Double]]->[[Double]]
 muta [] = []
 muta (x:xs) = (mutaAux x):(muta xs)
 
-mutaAux:: [Int] -> [Int]
-mutaAux xs 
-            | (sum xs) == 1 = []
-            | (sum xs) == 0 = []
- -- No completado
+mutaAux:: [Double] -> [Double]
+mutaAux xs  
+  | (sum xs) == 1 =  xs 
+  | otherwise = transforma (take (length xs) xs) 0.7
+ 
 
-fitness:: [Int] -> Int
-fitness xs 
-    | r < 45 = r
-    | otherwise = 9283489
+solucion :: Tablero -> SolucionReinas
+solucion xs = (fitness xs, xs)
+
+fitness :: [[Double]] -> Double
+fitness [] = 0
+fitness (x:xs) = fitness' x + fitness xs 
+
+fitness':: [Double] -> Double
+fitness' xs 
+    | r ==1 = r
+    | otherwise = 928348939244.00
     where r = sum xs
--- si hay 2 en una fila, penaliza mucho, si hay coincidencia de reinas, no tanto
+-- si hay 2 en una fila, penaliza mucho, si hay coincidencia de reinas, no tanto  
